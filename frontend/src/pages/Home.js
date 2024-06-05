7b8b627c80912a59bd143d088278332557cc8a1b
achref <jarbouiachraf899@gmail.com>
2024-06-05 21:26:21 +0200

    fetching data added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react"
+
+// components
+import WorkoutDetails from "../components/workoutDeetails"
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h1>Home</h1>
-        </div>
-    )
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+    </div>
+  )
 }
-export default Home;
+
+export default Home
